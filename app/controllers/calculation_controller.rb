982216ee14aa80ca83c8044a9bf25fad73af3162
James Hetherington <jamespjh@googlemail.com>
2011-02-16 09:44:12 +0000

    Clean controller debug

--- app/controllers/calculation_controller.rb
+++ app/controllers/calculation_controller.rb
@@ -5,17 +5,12 @@ class CalculationController < ApplicationController
 
   def enter
     @calculation=Calculations[params[:calculation]]
-    Rails.logger.debug(@calculation.inspect)
   end
 
   def result
     @calculation=Calculations[params[:calculation]].clone
-    @calculation.choose!(params['entry'])
-    Rails.logger.debug(@calculation.satisfied?)
-    Rails.logger.debug(@calculation.unset_drills)
+    @calculation.choose!(params['entry'])   
     @calculation.calculate!
-    Rails.logger.debug(@calculation.inspect)
-    
   end
   
 end
