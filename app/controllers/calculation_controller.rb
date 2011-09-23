f1a39774d8cce9b75802adc17c3735a8c04f29c0
spatchcock <andrew.berkeley.is@googlemail.com>
2011-09-23 22:32:20 +0100

    improve sort method

--- app/controllers/calculation_controller.rb
+++ app/controllers/calculation_controller.rb
@@ -177,11 +177,24 @@ module AMEE
     class TermsList
 
       def sort_by(attr)
-        # Remove unset terms before sort and append at end
-        unset_terms = TermsList.new(select { |term| term.unset? })
-        set_terms = TermsList.new(select { |term| term.set? }).standardize_units
-        set_terms.sort! { |term,other_term| term.send(attr) <=> other_term.send(attr) }
-        TermsList.new(set_terms + unset_terms)
+
+        # 1. Remove unset terms before sort and append at end
+        #
+        # 2. Establish set terms
+        #
+        # 3. Zip together with corresponding standardized units list creating a
+        # list of Term pairs
+        #
+        # 4. Sort list according to standardized Terms
+        #
+        # 5. Return map of original (now sorted) Terms
+
+        unset_terms, set_terms = self.partition { |term| term.unset? || term.value.nil? }
+        standardized_set_terms = TermsList.new(set_terms).standardize_units
+        ordered_set_terms = set_terms.zip(standardized_set_terms).sort! do |term,other_term|
+          term[1].send(attr) <=> other_term[1].send(attr)
+        end.map {|term_array| term_array[0]}
+        TermsList.new(ordered_set_terms + unset_terms)
       end
 
       def standardize_units(unit=nil,per_unit=nil)
